0ff0aee85ff516adf46dc775df10cc0211ba72e9
scotartt <scot.mcphee@gmail.com>
2014-04-03 23:55:49 +1000

    more generic

--- CTS_XML_TEI/perseus/get_data.sh
+++ CTS_XML_TEI/perseus/get_data.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-find greekLit -type f | xargs grep -l 'TEI P4//DTD' | grep 'perseus-grc' >greekLitTemp.txt
-find latinLit -type f | xargs grep -l 'TEI P4//DTD' | grep 'perseus-lat' >latinLitTemp.txt
+find greekLit -type f | xargs grep -l 'TEI P4//DTD' | grep 'grc' >greekLitTemp.txt
+find latinLit -type f | xargs grep -l 'TEI P4//DTD' | grep 'lat' >latinLitTemp.txt
 
-find greekLit -type f | xargs grep -l '<TEI.2>' | grep 'perseus-grc' >>greekLitTemp.txt
-find latinLit -type f | xargs grep -l '<TEI.2>' | grep 'perseus-lat' >>latinLitTemp.txt
+find greekLit -type f | xargs grep -l '<TEI.2>' | grep 'grc' >>greekLitTemp.txt
+find latinLit -type f | xargs grep -l '<TEI.2>' | grep 'lat' >>latinLitTemp.txt
 
-find greekLit -type f | xargs grep -l 'templates/tei-xl.rng' | grep 'perseus-grc' >>greekLitTemp.txt
-find latinLit -type f | xargs grep -l 'templates/tei-xl.rng' | grep 'perseus-lat' >>latinLitTemp.txt
+find greekLit -type f | xargs grep -l 'templates/tei-xl.rng' | grep 'grc' >>greekLitTemp.txt
+find latinLit -type f | xargs grep -l 'templates/tei-xl.rng' | grep 'lat' >>latinLitTemp.txt
 
 cat greekLitTemp.txt | sort -u > greekLit.txt 
 cat latinLitTemp.txt | sort -u > latinLit.txt
